567cc9ac9a55e96b0fb0cc62306be78306baf852
Hassan Saeed <hassansaeed0050@gmail.com>
2026-01-21 23:27:14 +0500

    Get requests done

--- FastAPI/Project 1- Books/Books.py
+++ FastAPI/Project 1- Books/Books.py
@@ -4,9 +4,9 @@ app = FastAPI()
 
 BOOKS = [
     {"title": "To Kill a Mockingbird", "author": "Harper Lee", "year": 1960, "genre": "Fiction", "isbn": "9780061120084"},
-    {"title": "1984", "author": "George Orwell", "year": 1949, "genre": "Dystopian", "isbn": "9780451524935"},
+    {"title": "1984", "author": "George Orwell", "year": 1949, "genre": "Romance", "isbn": "9780451524935"},
     {"title": "The Great Gatsby", "author": "F. Scott Fitzgerald", "year": 1925, "genre": "Classic", "isbn": "9780743273565"},
-    {"title": "Pride and Prejudice", "author": "Jane Austen", "year": 1813, "genre": "Romance", "isbn": "9780141439518"},
+    {"title": "Pride and Prejudice", "author": "George Orwell", "year": 1813, "genre": "Romance", "isbn": "9780141439518"},
     {"title": "The Hobbit", "author": "J.R.R. Tolkien", "year": 1937, "genre": "Fantasy", "isbn": "9780547928227"}
 ]
 
@@ -17,9 +17,28 @@ async def read_all_books():
 
 
 
-@app.get("/books/{book_title}")
+@app.get("/books/title/{book_title}")
 async def read_all_books(book_title: str):
     for book in BOOKS:
         if book.get('title').casefold() == book_title.casefold():
             return book
 
+
+
+@app.get("/book/")
+async def read_all_books(genre: str):
+    books_to_be_returned = []
+    for book in BOOKS:
+        if book.get('genre').casefold() == genre.casefold():
+            books_to_be_returned.append(book)
+    return books_to_be_returned
+
+@app.get("/books/author/{book_author}/")
+async def read_author_category_by_query(book_author: str , genre : str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('author').casefold() == book_author.casefold() and \
+            book.get('genre').casefold() == genre.casefold():
+            books_to_return.append(book)
+
+    return books_to_return
